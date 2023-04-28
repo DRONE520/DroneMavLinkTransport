17bfa8fa66f91376a0d5ed93b9ba317883a049e7
petrolingus <petrolingus@gmail.com>
2023-04-29 02:39:54 +0300

    feat: add compass data retrieval

--- main.cpp
+++ main.cpp
@@ -1,14 +1,34 @@
 #include <iostream>
-#include <thread>
 #include "include/MavLinkConnection.hpp"
+#include "include/MavLinkVehicle.hpp"
+#include "include/MavLinkMessages.hpp"
 
 int main() {
 
     std::cout << "DroneMavLinkTransport start" << std::endl;
 
-    auto droneConnection = mavlinkcom::MavLinkConnection::connectSerial("drone", "/dev/ttyACM0", 115200, "sh /etc/init.d/rc.usb\n");
-    auto proxyConnection = mavlinkcom::MavLinkConnection::connectRemoteUdp("qgc", "127.0.0.1", "127.0.0.1", 14550);
-    droneConnection->join(proxyConnection);
+    // Serial connect to PX4
+    auto droneConnection = mavlinkcom::MavLinkConnection::connectSerial("drone", "/dev/ttyS1", 57600, "sh /etc/init.d/rc.usb\n");
+
+    // Create MavLinkVehicle which associated with serial port
+    auto vehicle = std::make_shared<mavlinkcom::MavLinkVehicle>(0, -1);
+    vehicle->connect(droneConnection);
+    vehicle->startHeartbeat();
+
+    // Check vehicle heartbeat to make sure everything is okay
+    mavlinkcom::MavLinkHeartbeat heartbeat;
+    bool wait = vehicle->waitForHeartbeat().wait(10000, &heartbeat);
+    if (!wait) {
+        throw std::runtime_error("Received no heartbeat from PX4 after 10 seconds!");
+    }
+
+    // Get compass status
+    while(true) {
+        float pitch = vehicle->getVehicleState().attitude.pitch;
+        float yaw = vehicle->getVehicleState().attitude.yaw;
+        float roll = vehicle->getVehicleState().attitude.roll;
+        std::cout << "[pitch, yaw, roll]: " << pitch << " " << yaw << " " << roll << std::endl;
+    }
 
     return 0;
 }
