c04892b0b804d2473e14a2a809d6fe620d534234
petrolingus <petrolingus@gmail.com>
2023-06-02 13:51:48 +0300

    feat: remote addr as argument

--- main2.cpp
+++ main2.cpp
@@ -2,13 +2,14 @@
 #include "include/MavLinkConnection.hpp"
 #include "include/MavLinkVehicle.hpp"
 
-int main() {
+int main(int argc, char* argv[]) {
 
     std::cout << "Proxying is running..." << std::endl;
+    std::cout << "Remote address: " << argv[1] << std::endl;
 
     // Serial connect to PX4
     auto droneConnection = mavlinkcom::MavLinkConnection::connectSerial("drone", "/dev/ttyS1", 57600, "sh /etc/init.d/rc.usb\n");
-    auto proxyConnection = mavlinkcom::MavLinkConnection::connectRemoteUdp("qgc", "192.168.1.111", "192.168.1.139", 14550);
+    auto proxyConnection = mavlinkcom::MavLinkConnection::connectRemoteUdp("qgc", "192.168.1.111", argv[1], 14550);
     droneConnection->join(proxyConnection);
 
 
